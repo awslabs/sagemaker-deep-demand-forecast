430caf7017fa14cd50275696ad5603c702440941
ehsanmok <6980212+ehsanmok@users.noreply.github.com>
2020-04-24 21:39:44 +0000

    Allow regenerate data

--- src/deep_demand_forecast/data.py
+++ src/deep_demand_forecast/data.py
@@ -5,8 +5,8 @@ from gluonts.dataset.multivariate_grouper import MultivariateGrouper
 from gluonts.dataset.common import TrainDatasets
 
 
-def load_dataset(dataset_name: str, path: Path) -> TrainDatasets:
-    dataset = get_dataset(dataset_name, path, regenerate=False)
+def load_dataset(dataset_name: str, path: Path, regenerate=False) -> TrainDatasets:
+    dataset = get_dataset(dataset_name, path, regenerate)
     target_dim = dataset.metadata.feat_static_cat[0].cardinality
     grouper_train = MultivariateGrouper(max_target_dim=target_dim)
     grouper_test = MultivariateGrouper(max_target_dim=target_dim)
