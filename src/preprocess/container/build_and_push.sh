78c76c9e947d40dcd818d3e3e808dfe5d11ac8bb
Ehsan M. Kermani <6980212+ehsanmok@users.noreply.github.com>
2020-10-12 10:53:16 -0700

    Fix ecr get-login-password
    #26

--- src/preprocess/container/build_and_push.sh
+++ src/preprocess/container/build_and_push.sh
@@ -20,8 +20,9 @@ then
 fi
 
 # Get the login command from ECR and execute it directly
-$(aws ecr get-login --region ${region} --registry-ids ${account} --no-include-email)
-
+aws ecr get-login-password --region ${region} | docker login \
+     --username AWS \
+     --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com
 
 # Build the docker image locally with the image name and then push it to ECR
 # with the full name.
